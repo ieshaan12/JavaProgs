c068ab8ec177fd6e8b020a152977b98f2d04a95f
ieshaan12 <31623825+ieshaan12@users.noreply.github.com>
2018-09-18 23:53:07 +0530

    Changes in ArrayList

--- ArrayList/src/Al321.java
+++ ArrayList/src/Al321.java
@@ -17,6 +17,22 @@ while(itr.hasNext())
 	System.out.println(itr.next());
 while(itr.hasPrevious())
 	System.out.println(itr.previous());
-
+ArrayList g=new ArrayList();
+g.add("HELL");
+g.add(" AND ");
+g.add("HEAVEN");
+g.add("3");
+String s[]=new String[4];
+///for(int i=0;i<4;i++)
+//	s[i]=(String)g.get(i);
+System.out.println(g);
+//System.out.println(Arrays.toString(s));
+/*
+ Type-Safety: We can hold only a single type of objects in generics.
+  			  It doesn't allow to store other objects.
+ Type Casting: There is no need to typecast the object.
+ Compile Time Checking: It is checked at compile time so problem will 
+ 						not occur at runtime.
+ */
 }
 }
